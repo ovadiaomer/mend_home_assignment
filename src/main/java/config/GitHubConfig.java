d6ac965ac24b9eebb3f90ff739b6b40797d1c7ac
omerovadia <ovadia.omero@gmail.com>
2024-10-01 15:10:10 +0300

    remove token

--- src/main/java/config/GitHubConfig.java
+++ src/main/java/config/GitHubConfig.java
@@ -1,4 +1,14 @@
 package config;
 
 public class GitHubConfig {
+    public static final String BASE_URL = "https://api.github.com";
+    public static final String TOKEN = "github_pat_11ABKTDHI0vxQWrrMtOWWh_ukLWm3lYTH3BQo9tpAhO9xZdtTumSXcTugPmQz4vrir66UZWO55vCk9iHZ";
+    public static final String USERNAME = "ovadiaomer";
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "token ";
+    public static final String CONTENT_TYPE_HEADER = "Content-Type";
+    public static final String JSON_CONTENT_TYPE = "application/json";
+    public static final int STATUS_OK = 200;
+    public static final int STATUS_CREATED = 201;
+    public static final int STATUS_NO_CONTENT = 204;
 }
